d8e6c9915ef3bdcf180b0ec99d6fdc521d3bec72
David <contact.davidrobert@gmail.com>
2020-03-18 15:34:28 +0000

    request + response HTTP Foundation

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,6 +9,8 @@ namespace App\Controller;
 // je fais un "use" vers le namespace (qui correspond au chemin) de la classe "Route"
 // ça correspond à un import ou un require en PHP
 // pour pouvoir utiliser cette classe dans mon code
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 // je créé ma classe HomeController et je la nomme de la même manière que mon fichier
@@ -19,12 +21,33 @@ class HomeController
     // (ça s'appelle une annotation)
     // ce qui me permet de créer une url dans mon projet symfony
     // qui appelle le code de la méthode juste en dessous
+
     /**
      * @Route("/", name="home")
      */
-    public function homeAction()
+    public function home()
     {
-        var_dump('je suis bien sur la page home'); die;
+        // je stocke dans une variable tous les parametres de Request
+        // à savoir les données de la requête de l'utilisateur
+        $request = Request::createFromGlobals();
+
+        // dans les données de requête, je récupère uniquement le parametre d'url "age"
+        $age = $request->query->get('age');
+
+        // si l'âge est supérieur ou égal à 18, j'affiche un message d'accueil
+        if ($age >= 18 ) {
+            $message = 'Vous pouvez accéder à ce site !';
+        // sinon j'affiche un autre message
+        } else {
+            $message = 'Dégage morveux !';
+        }
+
+        // j'utilise la classe Response du composant HTTP Foundation pour créer une réponse
+        $response = new Response($message);
+
+        // je retourne la réponse créée
+        return $response;
+
     }
 
 }
