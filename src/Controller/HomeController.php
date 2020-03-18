5e87707531a5ddbfb01113e2e52e78c2487d44b2
David <contact.davidrobert@gmail.com>
2020-03-18 16:26:00 +0000

    wildcard

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -22,24 +22,28 @@ class HomeController
     // ce qui me permet de créer une url dans mon projet symfony
     // qui appelle le code de la méthode juste en dessous
 
+    // on créé un parametre d'url age avec symfony, alias une "wildcard"
+    // qu'on récupère ensuite en passant en parametre de la méthode une variable
+    // qui a le même nom que la "wildcard"
+
     /**
-     * @Route("/", name="home")
+     * @Route("/{age}", name="home")
      */
-    public function home()
+    public function home(Request $request, $age)
     {
         // je stocke dans une variable tous les parametres de Request
         // à savoir les données de la requête de l'utilisateur
-        $request = Request::createFromGlobals();
+        // $request = Request::createFromGlobals();
 
         // dans les données de requête, je récupère uniquement le parametre d'url "age"
-        $age = $request->query->get('age');
+        //$age = $request->query->get('age');
 
         // si l'âge est supérieur ou égal à 18, j'affiche un message d'accueil
         if ($age >= 18 ) {
-            $message = 'Vous pouvez accéder à ce site !';
+            $message = '<h1>Vous pouvez accéder à ce site !</h1>';
         // sinon j'affiche un autre message
         } else {
-            $message = 'Dégage morveux !';
+            $message = '<h1>Cass\' toi pauv\' con !</h1>';
         }
 
         // j'utilise la classe Response du composant HTTP Foundation pour créer une réponse
