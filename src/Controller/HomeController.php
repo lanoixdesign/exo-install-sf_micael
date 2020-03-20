19a1921ef0beec63ba154f6813ddcd422d5e2b76
David <contact.davidrobert@gmail.com>
2020-03-20 16:02:24 +0000

    extends twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -116,20 +116,38 @@ class HomeController extends AbstractController
             1 => [
                 'title' => 'titre article 1',
                 'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-                'categories' => ['politique', 'actualités'],
+                'image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMiF_QYKvu_SgiciSMJ7AwfSOl7I_VLJAC_23h9X102-sKZP_3',
                 'published' => true
             ],
             2 => [
                 'title' => 'titre article 2',
                 'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-                'categories' => ['cuisine', 'corona-virus'],
+                'image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMiF_QYKvu_SgiciSMJ7AwfSOl7I_VLJAC_23h9X102-sKZP_3',
                 'published' => true
             ],
             3 => [
                 'title' => 'titre article 3',
                 'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-                'categories' => ['monde'],
+                'image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMiF_QYKvu_SgiciSMJ7AwfSOl7I_VLJAC_23h9X102-sKZP_3',
                 'published' => false
+            ],
+            4 => [
+                'title' => 'titre article 4',
+                'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+                'image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMiF_QYKvu_SgiciSMJ7AwfSOl7I_VLJAC_23h9X102-sKZP_3',
+                'published' => false
+            ],
+            5 => [
+                'title' => 'titre article 5',
+                'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+                'image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMiF_QYKvu_SgiciSMJ7AwfSOl7I_VLJAC_23h9X102-sKZP_3',
+                'published' => true
+            ],
+            6 => [
+                'title' => 'titre article 6',
+                'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+                'image' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMiF_QYKvu_SgiciSMJ7AwfSOl7I_VLJAC_23h9X102-sKZP_3',
+                'published' => true
             ]
         ];
 
