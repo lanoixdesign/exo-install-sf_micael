be6a041334041a5508627ae8e9e6aeffcda691ee
David <contact.davidrobert@gmail.com>
2020-03-27 15:45:20 +0000

    display last books on home

--- src/Controller/Front/HomeController.php
+++ src/Controller/Front/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Front;
 
+use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,8 +11,20 @@ class HomeController extends AbstractController
 	/**
 	 * @Route("/", name="home")
 	 */
-	public function home()
+	public function home(BookRepository $bookRepository)
 	{
-		return $this->render( 'front/home.html.twig');
+	    
+        // METHODE 1 (pas optimisée)
+        // je récupére uniquement les deux derniers éléments de mon array de books
+        // donc les deux derniers livres
+        //$books = $bookRepository->findAll();
+        //$lastBooks = array_slice($books, -2, 2);
+
+        //METHODE 2
+        $lastBooks = $bookRepository->findBy([], ['id' => 'DESC'], 2, 0);
+
+		return $this->render( 'front/home.html.twig', [
+		    'books' => $lastBooks
+        ]);
 	}
 }
